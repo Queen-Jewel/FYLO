95c5cd4cd2b10a3eafedee282f946c508a1014fd
Jeweljeff <ketachi.jeff@gmial.com>
2024-01-26 15:41:05 +0100

    login page done

--- login.js
+++ login.js
@@ -1,13 +1,29 @@
-const button = document.getElementsByClassName("btn");
+const button = document.querySelector(".btn");
 
-loginIn = event => {
-    event.preventDefault();
+let loginIn = (e) => {
+e.preventDefault();
 
 
 let username = document.getElementById('email').value;
 let password = document.getElementById('password').value;
 
-alert('Login successful!');
+
+
+let success = () => {
+    
+    if(username.length === "" && password.length === "" ) {
+        alert('Please insert your details');
+    }else if(username === "" && password === ""){
+        alert('Insert your details');
+    }
+    else if(password.length <= 8 ){
+        alert('Not a strong password');
+    }else {
+        alert('Login successful!');
+    }
+}
+success();
+
 }
 
-button.addEventListener(('submit'), loginIn());
+button.addEventListener('click', loginIn);
