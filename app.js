f198ef5358b76c2c6088c6f7d534d218d2c09b8f
Jeweljeff <ketachi.jeff@gmial.com>
2024-01-31 00:56:49 +0100

    i think im done?

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const button = document.querySelector(".btn");
 const button2 = document.querySelector("#btn");
-const useremail = document.querySelector("#email").value;
 
 let getStart = (e) => {
   e.preventDefault();
+  const useremail = document.querySelector("#email").value;
 
-  if (useremail === "") {
+  if (!/@/.test(useremail)) {
     alert("Insert Email!");
   } else {
     window.location.href = "signup/sign.html";
@@ -15,8 +15,8 @@ let getStart = (e) => {
 button.addEventListener("click", getStart);
 let getStart2 = (e) => {
   e.preventDefault();
-
-  if (useremail === "") {
+  const useremail = document.querySelector("#email").value;
+  if (!/@/.test(useremail)) {
     alert("Insert Email!");
   } else {
     window.location.href = "signup/sign.html";
